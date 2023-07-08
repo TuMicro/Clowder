27cb1e0ff618cfc760c54ad4e382c60b5fa02870
josue.0 <josueajc@gmail.com>
2023-07-07 21:00:59 -0500

    comment on constant

--- test/constants/seaport.ts
+++ test/constants/seaport.ts
@@ -2,5 +2,5 @@
 export class OpenSeaSeaportConstants {
   static readonly SEAPORT_ADDRESS_1_5 = "0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC"; // canonical accross all chains
   static readonly CONDUIT_CONTROLLER = "0x00000000F9490004C11Cef243f5400493c00Ad63"; // canonical accross all chains
-  static readonly CONDUIT_KEY = "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000";
+  static readonly CONDUIT_KEY = "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000"; // from tests (https://docs.google.com/document/d/1oRXAKt9DSRsS5foRkvY4uQ0RmiwFsDxwptzfAZI_cmM/edit) seems to be cannonical too, but couldn't find docs about it
 }
